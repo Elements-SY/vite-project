1b20dc57726a3b88bb70bdfef173aa5f99c305a0
Elements-SY <yu.sun@erry.com>
2023-12-05 09:52:08 +0800

    modify

--- src/components/Layout/components/Hamburger/index.vue
+++ src/components/Layout/components/Hamburger/index.vue
@@ -1,11 +1,40 @@
 <template>
-  <div class="hamburger">
-    
-  </div>
+ <div
+  class="hamburger"
+  @click="toggleClick"
+ >
+  <svg-icon
+   class="hamburger-icon"
+   :class="{ 'is-active': isActive }"
+   icon-class="shrink"
+  />
+ </div>
 </template>
 <script setup lang="ts">
+defineProps({
+ isActive: {
+  required: true,
+  type: Boolean,
+  default: false,
+ },
+});
 
+const emit = defineEmits(['toggleClick']);
+
+function toggleClick() {
+ emit('toggleClick');
+}
 </script>
-<style lang="scss">
 
+<style scoped lang="scss">
+.hamburger {
+ vertical-align: middle;
+ cursor: pointer;
+ transform: scaleX(-1);
+ .hamburger-icon {
+ }
+ .is-active {
+  transform: scaleX(1);
+ }
+}
 </style>
